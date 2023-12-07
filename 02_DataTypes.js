9099b7012f95128eeb3439173ed22e7415bc490b
Jittub45 <jitendrakumaryadav2003@gmail.com>
2023-12-07 13:49:08 +0530

    dataTypes

---
+++ 02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello there!!")  **we are using ide not browser
+
+let name = "Jitendra"
+// string => ""
+
+let age = 20
+// number => 2 to power 53
+
+let isLoggedIn = false
+// boolean => true/false
+
+let GDP = 3898743985485
+// bigint
+
+let temp = null; 
+// nyll => standalone value
+
+let state; 
+// undefined => 
+console.log(typeof name, age, state, temp, isLoggedIn, GDP)
+console.log(typeof null, undefined)
+console.table([name, age, state, temp, isLoggedIn, GDP])
