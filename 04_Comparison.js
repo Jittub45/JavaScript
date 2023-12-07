8b1ea71157bbc8318a4f4ab8729514d8217aa93d
Jittub45 <jitendrakumaryadav2003@gmail.com>
2023-12-07 17:14:15 +0530

    Comparison

---
+++ 04_Comparison.js
@@ -0,0 +1,15 @@
+console.log(5 > 2)
+console.log(5 < 2)
+console.log(5 == 2)
+
+console.log(5 > 2)
+console.log("5" > 2)
+console.log(5 > "2")
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// ===  checks both data type and value
+console.log("2" == 2)
+console.log("2" === 2)
