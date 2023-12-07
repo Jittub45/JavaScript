67b995619412908eeedcf9caa2bd5b1dd88e2d01
Jittub45 <jitendrakumaryadav2003@gmail.com>
2023-12-07 13:08:10 +0530

    basic of javascript basic of javascript

---
+++ 01_Basic.js
@@ -0,0 +1,19 @@
+console.log("Jitendra")
+
+const accountId = 14123
+var accountPass;
+let accoutnEmail = "jitendra@google.com"
+accountCity = "Phagwara"
+
+// accountId = 6489             ** not allowed to change cont 
+// console.log(accountId)
+
+accoutnEmail = "jit@google.com"
+accountPass
+accountCity = "Hyderabad"
+
+console.table([accountId,accoutnEmail,accountPass,accountCity])
+
+
+// var is is not used because in previous day the scope{} was not compatable with var and it changed the var variable out the scope.
+
