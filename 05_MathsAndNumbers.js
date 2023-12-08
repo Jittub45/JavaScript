2f9780e660263e50c47ca05c0aa1d96c580de5c8
Jittub45 <jitendrakumaryadav2003@gmail.com>
2023-12-09 03:07:35 +0530

    Maths

---
+++ 05_MathsAndNumbers.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(900)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString())
+
+//*************************Maths**************************************
+console.log(Math)
+console.log(Math.abs(-45))
+console.log(Math.round(434.34))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(8.9))
+
+
+console.log(Math.random())
+console.log(Math.round(Math.random()*10)) // 0-10
+
