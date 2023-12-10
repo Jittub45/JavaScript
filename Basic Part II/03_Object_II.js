e448f93f14984b1e736cfa7ad80e18433a9f8ce8
Jittub45 <jitendrakumaryadav2003@gmail.com>
2023-12-10 16:51:39 +0530

    Object II

---
+++ Basic Part II/03_Object_II.js
@@ -0,0 +1,33 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+
+const regularUser = {
+    email: "user@google.com",
+    fullname: {
+        userfullname:{
+            firstname: "Jitendra",
+            lastname: "Kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = {obj1, obj2}
+console.log(obj3); 
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
